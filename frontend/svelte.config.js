8ef9d88902aa7b10e20595e65016d92118e1e9e6
samuelkalocaiEDU <ps.kalocai.sam@student.sostp.cz>
2026-04-03 20:52:55 +0200

    svelte adapter

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,15 +1,13 @@
-import adapter from '@sveltejs/adapter-auto'; // Vercel ho detekuje automaticky
+import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Tohle zajisti, ze Svelte bude rozumet tvemu CSS a modernimu zapisu
-	preprocess: vitePreprocess(),
+    preprocess: vitePreprocess(),
 
-	kit: {
-		// Tady rikas Sveltu: "Pouzij ten auto adapter, ktery jsem nainstaloval"
-		adapter: adapter()
-	}
+    kit: {
+        adapter: adapter()
+    }
 };
 
 export default config;
